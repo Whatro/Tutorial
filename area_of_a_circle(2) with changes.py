f186f2e62595aa0c30861aca72d3346cd05b07d8
Whatro <166838774+Whatro@users.noreply.github.com>
2024-04-13 17:39:40 -0500

    Add files via upload

---
+++ area_of_a_circle(2) with changes.py
@@ -0,0 +1,12 @@
+def compute_area_of_circle(radius):
+	pi = 3.14
+	area = pi * radius * radius
+	return area
+
+radius1 = 30
+area1 = compute_area_of_circle(radius1)
+print(f"The area of a circle with radius {radius1} is going to be: {area1}")
+
+radius2 = 40
+area2 = compute_area_of_circle(radius2)
+print(f"The area of a circle with radius {radius2} is going to be: {area2}")
